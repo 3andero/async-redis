115d97163ff49ca9068cede43167796eaf2ad0cd
3andero <3andero@github.com>
2021-01-15 20:11:07 +0800

    implmenting expiration

--- src/db.rs
+++ src/db.rs
@@ -4,9 +4,13 @@ use std::{
     collections::BTreeMap,
     sync::Arc,
     sync::{atomic::AtomicU64, Mutex},
+    unimplemented,
+};
+use tokio::{
+    select,
+    sync::mpsc,
     time::{Duration, Instant},
 };
-use tokio::{select, sync::mpsc};
 
 use dashmap::DashMap;
 
@@ -25,26 +29,24 @@ pub struct State {
 #[derive(Debug)]
 pub struct Shared {
     num_partition: usize,
-    states: Vec<State>,
+    state: State,
     counter: AtomicU64,
     tasks_tx: Vec<mpsc::Sender<(Instant, u64, String, usize)>>,
 }
 
 impl Shared {
     fn new(num_partition: usize) -> Self {
-        let mut tmp = vec![];
         let mut senders = vec![];
         for i in 0..num_partition {
-            tmp.push(State {
-                database: DashMap::new(),
-            });
             if i % 10 == 0 {
                 // senders.push()
             }
         }
         Self {
             num_partition,
-            states: tmp,
+            state: State {
+                database: DashMap::new(),
+            },
             counter: AtomicU64::new(0),
             tasks_tx: senders,
         }
@@ -72,7 +74,7 @@ impl DB {
 
     pub fn get(&self, key: &String) -> Option<Bytes> {
         let partition_id = self.shared.partition_id(&key);
-        let db = &self.shared.states[partition_id].database;
+        let db = &self.shared.state.database;
         db.get(key).map(|v| v.data.clone())
     }
 
@@ -82,8 +84,8 @@ impl DB {
             .counter
             .fetch_add(1, std::sync::atomic::Ordering::Relaxed);
 
-        let partition_id = self.shared.partition_id(&key);
-        let db = &self.shared.states[partition_id].database;
+        let db = &self.shared.state.database;
+        let partition_id = db.determine_map(&key);
         db.insert(
             key,
             Entry {
@@ -99,16 +101,44 @@ async fn purge_expired_keys(
     states: &Vec<State>,
     mut tasks_rx: mpsc::Receiver<(Instant, u64, String, usize)>,
 ) {
-    let when: Option<Instant> = None;
+    let mut when: Option<Instant> = None;
     let expirations: BTreeMap<(Instant, u64), (String, usize)> = BTreeMap::new();
 
     loop {
+        let now = Instant::now();
+
         // select! {
         let res = tasks_rx.recv().await.unwrap();
         {
-            match when {
-                Some(time) => if res.0 < time {},
-                None => (),
+            let now = Instant::now();
+            when = match when {
+                Some(v) => {
+                    if res.0 < v && res.0 > now {
+                        expirations.insert((res.0, res.1), (res.2, res.3));
+                        Some(res.0)
+                    } else if res.0 < now {
+                        unimplemented!(); // purge expired keys.
+                        None
+                    } else {
+                        expirations.insert((res.0, res.1), (res.2, res.3));
+                        Some(v)
+                    }
+                }
+                None => Some(res.0),
+            };
+        }
+        let _ = tokio::time::sleep(
+            when.map(|v| v.min(now + Duration::new(3, 0)))
+                .unwrap_or_default(now + Duration::MAX),
+        );
+
+        {
+            let now = Instant::now();
+            let keys = vec![];
+            while let Some((&(next_expire, _), (key, partition_id))) = expirations.iter().next() {
+                if next_expire <= now {
+                    
+                }
             }
         }
         // }
